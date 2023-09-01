c8196fd49c71958cee56c668199339d2396c53ff
jotavital <joaopedrovitaldoctum@gmail.com>
2023-09-01 12:46:03 -0300

    progress bar and fixes

--- colors.py
+++ colors.py
@@ -3,3 +3,4 @@ class bcolors:
     WARNING = '\033[93m'
     DANGER = '\033[91m'
     ENDC = '\033[0m'
+    LIGHT_BLUE = '\033[1;34m'
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import sys
-from os import listdir, rename
+from os import listdir, rename, system
 from os.path import isfile, join, splitext
 from PIL import Image
 import colors
 from datetime import datetime
+from progress.bar import ChargingBar
+from time import sleep
 
-photosPath = "./example-photos"
+photosPath = 'C:\\Users\\picle\\Desktop\\organize-photos\\example-photos'
 files = [f for f in listdir(photosPath) if isfile(join(photosPath, f))]
+totalFiles = len(files)
 
-decision = input(f'{colors.bcolors.WARNING}{len(files)} arquivos encontrados para processar, deseja continuar? (Y/n) {colors.bcolors.ENDC}') or 'Y'
+decision = input(
+    f'{colors.bcolors.WARNING}{totalFiles} arquivos encontrados para processar, deseja continuar? (Y/n) {colors.bcolors.ENDC}') or 'Y'
 
 if decision.upper() != 'Y':
     print(f'{colors.bcolors.DANGER}Adeus...')
     sys.exit()
 
+bar = ChargingBar(f'Processando ', max=totalFiles)
+bar.color = "blue"
+
+filesRenamed = 0
 for fileName in files:
+    bar.bar_prefix = f'{fileName}...'
+    bar.suffix = f'%(percent)d%% {bar.elapsed}s'
+
     filePath = f'{photosPath}/{fileName}'
     fileExtension = splitext(filePath)[1]
     file = Image.open(f'{filePath}')
@@ -23,13 +34,26 @@ for fileName in files:
 
     if not exif:
         print(f'{colors.bcolors.DANGER}ERRO: O arquivo {fileName} não contém o metadado. {colors.bcolors.ENDC}')
+        bar.next()
+        break
 
     dateTaken = exif[306]
     formattedDateTaken = datetime.strptime(dateTaken, '%Y:%m:%d %H:%M:%S')
-    formattedDateTaken = formattedDateTaken.strftime('%d-%m-%Y %H:%M:%S')
+    formattedDateTaken = formattedDateTaken.strftime('%d-%m-%Y %H-%M-%S')
 
-    newFileName = f'a{fileExtension}'
+    newFileName = f'{formattedDateTaken}{fileExtension}'
     newFilePath = f'{photosPath}/{newFileName}'
 
+    repeatedFileCounter = 0
+    while isfile(newFilePath) == 1:
+        repeatedFileCounter += 1
+        newFileName = f'{formattedDateTaken}-{repeatedFileCounter}{fileExtension}'
+        newFilePath = f'{photosPath}/{newFileName}'
+
     rename(f'{filePath}', f'{newFilePath}')
-    print(f'{colors.bcolors.SUCCESS}Processado: {fileName} -> {newFileName} {colors.bcolors.ENDC}')
+    filesRenamed += 1
+    sleep(0.5)
+    bar.next()
+
+bar.finish()
+print(f"{colors.bcolors.SUCCESS}{filesRenamed} arquivos renomeados em {bar.elapsed} segundos.{colors.bcolors.ENDC}")
