b612ebe6262ada8543e0a2791f0a5de4cfa7cd5c
jotavital <joaopedrovitaldoctum@gmail.com>
2023-09-01 00:42:11 -0300

    going to windows

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from os import listdir
+from os.path import isfile, join
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+photosPath = "./example-photos"
+onlyfiles = [f for f in listdir(photosPath) if isfile(join(photosPath, f))]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(onlyfiles)
