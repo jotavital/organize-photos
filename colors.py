4dc8d44621585b2d4cfed0c60d0ea69a7d97aee4
jotavital <joaopedrovitaldoctum@gmail.com>
2023-09-01 01:44:07 -0300

    rename files

---
+++ colors.py
@@ -0,0 +1,5 @@
+class bcolors:
+    SUCCESS = '\033[92m'
+    WARNING = '\033[93m'
+    DANGER = '\033[91m'
+    ENDC = '\033[0m'
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from os import listdir
-from os.path import isfile, join
+import sys
+from os import listdir, rename
+from os.path import isfile, join, splitext
+from PIL import Image
+import colors
+from datetime import datetime
 
 photosPath = "./example-photos"
-onlyfiles = [f for f in listdir(photosPath) if isfile(join(photosPath, f))]
+files = [f for f in listdir(photosPath) if isfile(join(photosPath, f))]
 
-print(onlyfiles)
+decision = input(f'{colors.bcolors.WARNING}{len(files)} arquivos encontrados para processar, deseja continuar? (Y/n) {colors.bcolors.ENDC}') or 'Y'
+
+if decision.upper() != 'Y':
+    print(f'{colors.bcolors.DANGER}Adeus...')
+    sys.exit()
+
+for fileName in files:
+    filePath = f'{photosPath}/{fileName}'
+    fileExtension = splitext(filePath)[1]
+    file = Image.open(f'{filePath}')
+    exif = file.getexif()
+    file.close()
+
+    if not exif:
+        print(f'{colors.bcolors.DANGER}ERRO: O arquivo {fileName} não contém o metadado. {colors.bcolors.ENDC}')
+
+    dateTaken = exif[306]
+    formattedDateTaken = datetime.strptime(dateTaken, '%Y:%m:%d %H:%M:%S')
+    formattedDateTaken = formattedDateTaken.strftime('%d-%m-%Y %H:%M:%S')
+
+    newFileName = f'a{fileExtension}'
+    newFilePath = f'{photosPath}/{newFileName}'
+
+    rename(f'{filePath}', f'{newFilePath}')
+    print(f'{colors.bcolors.SUCCESS}Processado: {fileName} -> {newFileName} {colors.bcolors.ENDC}')
